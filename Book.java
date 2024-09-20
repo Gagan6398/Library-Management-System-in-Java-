d896036186a4259ad24199999b255118a9e6905a
Gagan6398 <128702992+Gagan6398@users.noreply.github.com>
2024-09-20 15:53:51 +0530

    Book.java

---
+++ Book.java
@@ -0,0 +1,51 @@
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private boolean available;
+
+    public Book(int id, String title, String author, boolean available) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.available = available;
+    }
+
+    // Getter and Setter methods
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Title: " + title + ", Author: " + author + ", Available: " + available;
+    }
+}
